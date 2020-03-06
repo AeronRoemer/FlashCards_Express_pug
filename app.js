cfb1eda04d6db570ca5d658f0bbe41d3a94e9346
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-03-05 20:28:02 -0700

    Basic Pub Templates

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const colors = [
   
 app.set('view engine', 'pug');
 app.get('/', (req, res) =>{
+    res.locals.colors = colors;
     res.render('index');
 })
 app.get('/card', (req, res) =>{
     res.locals.prompt = 'Whatever';
     res.locals.Name = 'Nerd';
-    res.locals.colors = colors;
     res.render('card');
 });
 app.listen(3000, () =>{

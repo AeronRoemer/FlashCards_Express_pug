36d0ab99be103a3dd130f5cd492ba2d45a276a35
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-03-05 19:52:26 -0700

    pug stuff

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+  ];
+  
+app.set('view engine', 'pug');
 app.get('/', (req, res) =>{
-    res.send('Whatever');
+    res.render('index');
 })
-app.get('/hello', (req, res) =>{
-    res.send('<h1>Hello</h1>Whatever');
+app.get('/card', (req, res) =>{
+    res.locals.prompt = 'Whatever';
+    res.locals.Name = 'Nerd';
+    res.locals.colors = colors;
+    res.render('card');
 });
 app.listen(3000, () =>{
     console.log('The server is up and running')

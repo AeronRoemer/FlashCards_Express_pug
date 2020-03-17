8745c7869448b2b7ac494c40910eb37f16e35a64
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-03-16 19:32:24 -0600

    Cookie set

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+//requires express and a body-parser middleware for response objects
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const colors = [
     'red',
@@ -8,17 +11,48 @@ const colors = [
     'blue',
     'purple'
   ];
-  
+//sets the view engine as pug for render requests
 app.set('view engine', 'pug');
+//sets up the bodyParser so HTML form can post to the response body
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+//get requests for routes
 app.get('/', (req, res) =>{
     res.locals.colors = colors;
-    res.render('index');
+    const name = req.cookies.username;
+    //redirect to Hello page if cookie is not set
+    if (name){
+        res.render('index', {name: name});
+    } else {
+        res.redirect('/hello');
+    }
+    
 })
 app.get('/card', (req, res) =>{
     res.locals.prompt = 'Whatever';
     res.locals.Name = 'Nerd';
     res.render('card');
 });
+app.get('/hello', (req, res) =>{
+    const name = req.cookies.username;
+    if (name){
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
+    
+});
+
+//post requests for routes
+app.post('/hello', (req, res) =>{
+    //sets cookie
+    res.cookie('username', req.body.username);
+    //residrects on response
+    res.redirect('/');
+});
+
+//starts listening on specfied port(localhost:3000). Server must be started with node app.js in terminal
 app.listen(3000, () =>{
     console.log('The server is up and running')
 });

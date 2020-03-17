aec424bd8114946c26494299e4825002e8844bbd
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-03-16 20:56:48 -0600

    404 added

--- app.js
+++ app.js
@@ -15,7 +15,22 @@ const colors = [
 app.set('view engine', 'pug');
 //sets up the bodyParser so HTML form can post to the response body
 app.use(bodyParser.urlencoded({extended: false}));
+//sets up cookie Parser to user cookies
 app.use(cookieParser());
+
+//creates error
+app.use((req, res, next)=>{
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+})
+//error handler
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error', err);
+  });
+  
 
 //get requests for routes
 app.get('/', (req, res) =>{
@@ -51,6 +66,12 @@ app.post('/hello', (req, res) =>{
     //residrects on response
     res.redirect('/');
 });
+
+//resets cookies and redirects to form page
+app.post('/goodbye', (req, res)=>{
+    res.clearCookie('username');
+    res.redirect('/hello')
+})
 
 //starts listening on specfied port(localhost:3000). Server must be started with node app.js in terminal
 app.listen(3000, () =>{
